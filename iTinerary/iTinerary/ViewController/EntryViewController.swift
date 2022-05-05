80ff9cea8f12251382073529f79b6629b1bb50a7
Melissa <mho10@mail.sfsu.edu>
2022-05-05 15:49:10 -0700

    added note taking view controllers

---
+++ iTinerary/iTinerary/ViewController/EntryViewController.swift
@@ -0,0 +1,8 @@
+//
+//  EntryViewController.swift
+//  iTinerary
+//
+//  Created by Ze Lei on 5/5/22.
+//
+
+import Foundation
---
+++ iTinerary/iTinerary/ViewController/NoteViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EntryViewController.swift
+//  iTinerary
+//
+//  Created by Melissa Ho on 5/5/22.
+//
+// resource: https://www.youtube.com/watch?v=pZVXENscpTM
+
+import Foundation
+import UIKit
+
+class NoteViewController: UIViewController{
+    
+}
