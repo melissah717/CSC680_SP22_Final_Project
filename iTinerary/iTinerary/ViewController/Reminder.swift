2b858507b7ca18749698eece26cb8cae9629dc40
Sami <67216252+saxgetty@users.noreply.github.com>
2022-05-08 18:10:19 -0700

    Added Schedule with issues to fix

---
+++ iTinerary/iTinerary/ViewController/Reminder.swift
@@ -0,0 +1,44 @@
+//
+//  Reminder.swift
+//  iTinerary
+//
+//  Created by Samantha Saxton-Getty on 5/8/22.
+//
+
+import SwiftUI
+
+struct Reminder: Identifiable {
+    var id = UUID().uuidString
+    var title: String
+    var time: Date = Date()
+}
+
+struct ReminderData: Identifiable {
+    var id = UUID().uuidString
+    var remind: [Reminder]
+    var remindDate: Date
+}
+
+func getSampleDate(offset: Int) -> Date {
+    let calendar = Calendar.current
+    
+    let date = calendar.date(byAdding: .day, value: offset, to: Date())
+    
+    return date ?? Date()
+}
+
+var tasks: [ReminderData] = [
+    
+    ReminderData(remind: [
+    
+        Reminder(title: "Study for 680 Final"),
+        Reminder(title: "Finish 648 Final Project")
+    ], remindDate: getSampleDate(offset: -15)),
+    
+    ReminderData(remind: [
+    
+        Reminder(title: "Buy Groceries"),
+        Reminder(title: "Take the Dog to Vet")
+    ], remindDate: getSampleDate(offset: 4))
+]
+
